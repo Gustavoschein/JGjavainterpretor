18df5b077f47cfa5b97959067e7111a5d3ca9f64
João Marcos Campagnolo <jota.campagnolo@gmail.com>
2014-09-29 20:27:36 -0300

    Adicionado main.java com cabeçalho.

---
+++ fonte/main.java
@@ -0,0 +1,12 @@
+/*
+~#################################################################################~
+	Autores do código: 		Gustavo André Schein, João Marcos Campagnolo.
+	E-mail do autor: 		gustavoschein@hotmail.com, jota.campagnolo@gmail.com.
+	Propósito do programa:	O programa simula um interpretador JAVA de uma
+							linguagem de programação inventada pelos autores,
+							a qual recebe o código a ser interpretado através 
+							de um arquivo de texto, que será redirecionado como
+							entrada do programa.
+	Link do código fonte:	https://github.com/Gustavoschein/JGjavainterpretor
+~#################################################################################~
+*/
