a413c3e12c2f858b260fec7f90d746014ca82cd6
Gustavoschein <gustavoschein@hotmail.com>
2014-10-02 09:01:14 -0300

    Criação e declaração de variáveis.

---
+++ fonte/variavel.java
@@ -0,0 +1,42 @@
+﻿/*
+~#################################################################################~
+	Autores do código: 		Gustavo André Schein, João Marcos Campagnolo.
+	E-mail do autor: 		gustavoschein@hotmail.com, jota.campagnolo@gmail.com.
+	Propósito do programa:	O programa simula um interpretador JAVA de uma
+							linguagem de programação inventada pelos autores,
+							a qual recebe o código a ser interpretado através 
+							de um arquivo de texto, que será redirecionado como
+							entrada do programa.
+	Link do código fonte:	https://github.com/Gustavoschein/JGjavainterpretor
+~#################################################################################~
+*/
+
+	Class Variavel {
+		private String Nome; //Atributo
+		private double valor; //Atributo
+	
+		//  Métodos Construtores
+	
+		public Variavel (){
+			this.Nome = new String();
+			this.Valor = 0.0;
+		}
+		
+
+		//  Métodos gets e sets.
+	
+		public void setNome(String s){
+			this.Nome = s;
+		}
+		
+		public String getNome(){
+			return this.Nome;
+		}
+
+		public void setValor(double d){
+			this.Valor = d;
+		}
+
+		public double getValor(){
+			return this.Valor;
+		}
