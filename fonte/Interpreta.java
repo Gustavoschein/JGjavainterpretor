e0135f26957b17b790ed2ef6d85eccd8b603ce57
João Marcos Campagnolo <jota.campagnolo@gmail.com>
2014-10-19 13:45:28 -0200

    Adicionada a função de desempilhar...

--- fonte/Interpreta.java
+++ fonte/Interpreta.java
@@ -17,13 +17,11 @@ class Interpretador {
 	// ATRIBUTOS:    
 	private String linhas[];
 	private Variavel[] variaveis;
-	public int indexVar = -1; //indíce p/ saber topo da Vetor de Variaveis
+	public int indexVar = -1; // Índice usado para saber o Topo do vetor das variáveis:
 	public String [] pilha;	
 	
-	// CONSTRUTORES:		
-	
+	// CONSTRUTORES:	
 	public Interpretador(){	
-			
 		variaveis = new Variavel[200];
 		for (int www = 0;www<200 ;www++ ) {
 			this.variaveis[www] =  new Variavel();
@@ -31,7 +29,7 @@ class Interpretador {
 	}
 	
 	// MÉTODOS:
-	public void interpreta(String l[]){// funcao interpreta
+	public void interpreta(String l[]){ // Função que Interpreta:
 		String [] linhas ;
 		linhas = l;
 		int i, j = 0; 
@@ -63,5 +61,99 @@ class Interpretador {
 			
 		}
 		return Double.parseDouble(nomeVar);// caso variavel nao exista converte double		
+	}
+	
+	// ======================================================================================//
+	public boolean desempilha (){// faz todas as operações	
+		double total = 0;
+		double var1,var2;
+		for (int op = 0; op < pilha.length; op++){
+			//(  || ) 
+			if (pilha[op].equals("+")) { // SOMA
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				total = var1 + var2;
+				pilha[op+1] = String.valueOf(total);// converte p/string denovo	
+			
+			}else if (pilha[op].equals("-")) { // SUBTRAÇÃO
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				total = var1 - var2;
+				pilha[op+1] = String.valueOf(total);// converte p/string denovo	
+			
+			}else if (pilha[op].equals("*")) { //MULTIPLICAÇÃO
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				total = var1 * var2;
+				pilha[op+1] = String.valueOf(total);// converte p/string denovo	
+			
+			}else if((pilha[op].equals("%"))){ //PEGA O RESTO
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				total = var1 % var2;
+				pilha[op+1] = String.valueOf(total);// converte p/string denovo	
+
+			}else if (pilha[op].equals("/")){ //DIVISÃO
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				total = var1 / var2;
+				pilha[op+1] = String.valueOf(total);// converte p/string denovo	
+			
+			} else if (pilha[op].equals("<")) { //MENOR
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);				
+				if(var1 < var2) {
+					return true;
+				}else{
+					return false;
+				}
+			
+			} else if (pilha[op].equals(">")) { //MAIOR
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);				
+				if(var1 > var2) {
+					return true;
+				}else{
+					return false;
+				}
+			
+			} else if (pilha[op].equals("!=")) {// DIFERENTE
+				
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				if(var1 == var2){
+					return false;
+				}else{
+					return true;
+				}
+			}else if (pilha[op].equals("==")) {// IGUAL 				
+				var1 = converteDouble(pilha[op-1]);
+				var2 = converteDouble(pilha[op+1]);
+				if(var1 == var2){
+					return true;
+				}else{
+					return false;
+				}
+			}else if (pilha[op].equals("++")) {// IGUAL 				
+				var1 = converteDouble(pilha[op-1]);
+				total = var1 + 1;
+				pilha[op] = "=";// para que a verificação de atribuição localize atribuição	
+			}else if (pilha[op].equals("--")) {// IGUAL 				
+				var1 = converteDouble(pilha[op-1]);
+				total = var1 - 1;
+				pilha[op] = "=";// para que a verificação de atribuição localize atribuição	
+			}
+		}
+		for (int j = 0; j < pilha.length ; j ++) {// verif. se ha (=) atribuição
+			if (pilha[j].equals("=")) {	
+				int indiVAr = existeVariavel(pilha[j-1]);// pega indice da variavel no vetor de variaveis	
+				variaveis[indiVAr].setValor(total);
+			}
+			/* RESUMO : J VAI PARAR QUANDO TIVE ENCONTRADO =, A VARIAVEL QUE ESTA ANTES DO =  E 
+						 QUEM VAI RECEBE A ATRIBUICAO, E O VALOR VAI ESTAR EM TOTAL, POR QUE I PERCOREU
+						 TODA A PILHA E ESTA FORA, DAI -1. E SE NAO HOUVER IGUAL NAO ATRIBUI
+			*/
+		}	
+		return true;
 	}
 }	
